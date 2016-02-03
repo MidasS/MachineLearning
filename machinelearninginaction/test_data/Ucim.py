d2905681bfb1ec8529ebc2ecbd50bb65c34fef85
blue1028 <blue1028@unist.ac.kr>
2016-02-03 17:40:53 +0900

    update 222222

--- machinelearninginaction/test_data/Ucim.py
+++ machinelearninginaction/test_data/Ucim.py
@@ -5,18 +5,31 @@ import pandas as pd
 from matplotlib import style
 style.use("ggplot")
 
-def Build_Data_Set(features = ["width",
-                               "avg_T","std_T","rel_max_T","rel_min_T"]):
-    data_df = pd.DataFrame.from_csv("ng_data.csv")
-    # print(data_df)
 
-    X = np.array(data_df[features].values)
+
+# FEATURES = ['width','avg_T','std_T','after_T[-6]','after_T[-5]','after_T[-4]','after_T[-3]','after_T[-2]','after_T[-1]','after_T[0]','after_T[1]','after_T[2]','after_T[3]','after_T[4]','after_T[5]','after_T[6]', 'before_T[-6]', 'before_T[-5]', 'before_T[-4]', 'before_T[-3]', 'before_T[-2]', 'before_T[-1]', 'before_T[0]', 'before_T[1]', 'before_T[2]', 'before_T[3]', 'before_T[4]', 'before_T[5]', 'before_T[6]']
+FEATURES = ['avg_T','std_T','rel_T[-6]','rel_T[-5]','rel_T[-4]','rel_T[-3]','rel_T[-2]','rel_T[-1]','rel_T[0]','rel_T[1]','rel_T[2]','rel_T[3]','rel_T[4]','rel_T[5]','rel_T[6]']
+
+
+
+
+def Build_Data_Set():
+    data_df = pd.DataFrame.from_csv("ng_data_train.csv")
+    data_df = data_df.reindex(np.random.permutation(data_df.index))
+
+    X = np.array(data_df[FEATURES].values)
     X = preprocessing.scale(X)
-    print(X)
     y = (data_df["result_2"].values.tolist())
-    print(y)
-    print(y.count(1))
-    return X,y
+
+    data_df2 = pd.DataFrame.from_csv("ng_data_test.csv")
+    data_df2 = data_df2.reindex(np.random.permutation(data_df2.index))
+
+    X2 = np.array(data_df2[FEATURES].values)
+    X2 = preprocessing.scale(X2)
+    y2 = (data_df2["result_2"].values.tolist())
+
+
+    return X,y,X2,y2
 
 def Analysis():
     X, y = Build_Data_Set()
@@ -28,15 +41,75 @@ def Analysis():
     a = -w[0] / w[1]
     xx = np.linspace(min(X[:, 0]), max(X[:, 0]))
     yy = a * xx - clf.intercept_[0] / w[1]
-    print(xx,yy)
+    # print(xx,yy)
     h0 = plt.plot(xx,yy, "k-", label="non weighted")
 
     plt.scatter(X[:, 0],X[:, 1],c=y)
     plt.ylabel("avg_T")
     plt.xlabel("width")
-
     plt.legend()
 
     plt.show()
 
-Analysis()
+# Analysis()
+
+def Analysis2():
+
+    test_size = 1500
+    X, y,X2,y2 = Build_Data_Set()
+    print(len(X2[:-test_size]))
+
+    # print(X[:])
+    clf = svm.SVC(kernel="linear", C= 1.0)
+    clf.fit(X[:-test_size],y[:-test_size])
+    # print(clf.fit(X[:-test_size],y[:-test_size]))
+
+    correct_count = 0
+
+
+    for x in range(1, test_size+1):
+        # print(len(clf.predict(X[-x])))
+        if clf.predict(X[-x])[0] == y[-x]:
+            correct_count += 1
+
+    print("Accuracy:", (correct_count/test_size) * 100.0000)
+
+def Analysis3(kk,cc,gg):
+
+    X, y,X2,y2 = Build_Data_Set()
+
+
+    clf = svm.SVC(kernel=kk, C= cc, gamma=gg)
+    clf.fit(X[:],y[:])
+    correct_count = 0
+
+    for x in range(1, len(X2[:])):
+        if clf.predict(X2[x])[0] == y2[x]==0:
+            correct_count += 1
+
+    # print("Accuracy:", (correct_count/len(y2)) * 100.00)
+
+    print(correct_count)
+    print(len(y2))
+    # print(y2.count(0))
+    print("Accuracy:", (correct_count/y2.count(0)) * 100.00)
+
+
+class test():
+
+    def __init__(self):
+
+        list_k = ['rbf', 'poly','sigmoid']
+        print(list_k)
+        list_c = np.arange(10,500,40)
+        list_g = np.arange(0.001,0.02,0.002)
+
+        for i in list_k:
+            for j in list_c:
+                for k in list_g:
+                    print(i,j,k,':',Analysis3(i,j,k))
+
+
+test()
+
+# Analysis3('rbf',10,1)
--- machinelearninginaction/test_data/ucim_Test.py
+++ machinelearninginaction/test_data/ucim_Test.py
@@ -1,40 +1,10 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import svm
-#from sklearn.linear_model import SGDClassifier
+from urllib3 import Request, urlopen
+from urllib import urlencode, quote_plus
 
-# we create 40 separable points
-rng = np.random.RandomState(0)
-n_samples_1 = 1000
-n_samples_2 = 100
-X = np.r_[1.5 * rng.randn(n_samples_1, 2),
-          0.5 * rng.randn(n_samples_2, 2) + [2, 2]]
-y = [0] * (n_samples_1) + [1] * (n_samples_2)
-print(X)
-#
-# # fit the model and get the separating hyperplane
-# clf = svm.SVC(kernel='linear', C=1.0)
-# clf.fit(X, y)
-#
-# w = clf.coef_[0]
-# a = -w[0] / w[1]
-# xx = np.linspace(-5, 5)
-# yy = a * xx - clf.intercept_[0] / w[1]
-#
-#
-# # get the separating hyperplane using weighted classes
-# wclf = svm.SVC(kernel='linear', class_weight={1: 10})
-# wclf.fit(X, y)
-#
-# ww = wclf.coef_[0]
-# wa = -ww[0] / ww[1]
-# wyy = wa * xx - wclf.intercept_[0] / ww[1]
-#
-# # plot separating hyperplanes and samples
-# h0 = plt.plot(xx, yy, 'k-', label='no weights')
-# h1 = plt.plot(xx, wyy, 'k--', label='with weights')
-# plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired)
-# plt.legend()
-#
-# plt.axis('tight')
-# plt.show()
+url = 'http://api.seibro.or.kr/openapi/service/StockSvc/getNewDepoSecnList'
+queryParams = '?' + urlencode({ quote_plus('ServiceKey') : '서비스키', quote_plus('yyyymm') : '201301', quote_plus('searchType') : '2', quote_plus('issucoCustno') : '8424', quote_plus('numOfRows') : '999', quote_plus('pageNo') : '1' })
+
+request = Request(url + queryParams)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read()
+print (response_body)
