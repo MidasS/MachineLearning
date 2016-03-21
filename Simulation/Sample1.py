7c19ac0175577521ffe6b71cce89857f20276c56
blue1028 <blue1028@unist.ac.kr>
2016-03-21 18:11:26 +0900

    3344555

--- Simulation/Sample1.py
+++ Simulation/Sample1.py
@@ -3,8 +3,44 @@ import matplotlib.pyplot as plt
 from sklearn import svm, preprocessing
 import pandas as pd
 from matplotlib import style
+import math
 
 
-data_df = pd.DataFrame.from_csv("TimeTablef.csv")
+df = pd.DataFrame.from_csv("TimeTablerf.csv")
+for x in df.columns:
+    print(x)
 
-print(data_df)
+print()
+Bdf = df['Birthrate'].values
+Ddf = df['Deathrate'].values
+# print(type(Bdf))
+#
+#
+#
+# print(int(Bdf))
+
+
+def pearson(x,y):
+    n=len(x)
+    vals=range(n)
+    # 합한다.
+    sumx = sum([float(x[i]) for i in vals])
+    sumy = sum([float(y[i]) for i in vals])
+
+    # 제곱을 합한다.
+    sumxSq = sum([x[i]**2.0 for i in vals])
+    sumySq = sum([y[i]**2.0 for i in vals])
+
+    # 곱을 합한다.
+    pSum = sum([x[i]*y[i] for i in vals])
+
+    # 피어슨 점수를 계산한다.
+    num = pSum - (sumx*sumy/n)
+    den = ((sumxSq-pow(sumx,2)/n)*(sumySq-pow(sumy,2)/n))**.5
+    if den==0: return 0
+
+    r = num/den
+
+    return r
+
+print(pearson(Bdf,Ddf))
