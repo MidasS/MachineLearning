78f6f535d5158c657527d372775e0cdd5f0bd2a9
Song dong hwan <blue1028@unist.ac.kr>
2017-02-24 17:18:43 +0900

    Tensorflow 테스트 내용 추가

--- Keras/Keras Test.py
+++ Keras/Keras Test.py
@@ -1,8 +1,31 @@
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 import numpy as np
-import matplotlib.pyplot as plt
 
-s = np.sin(2* np.pi* 0.125* np.arange(20))
-plt.plot(s, 'ro-')
-plt.xlim(-0.5, 20.5)
-plt.ylim(-1.1, 1.1)
-plt.show()
+data_dim = 16
+timesteps = 8
+nb_classes = 10
+
+# expected input data shape: (batch_size, timesteps, data_dim)
+model = Sequential()
+model.add(LSTM(32, return_sequences=True,
+               input_shape=(timesteps, data_dim)))  # returns a sequence of vectors of dimension 32
+model.add(LSTM(32, return_sequences=True))  # returns a sequence of vectors of dimension 32
+model.add(LSTM(32))  # return a single vector of dimension 32
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# generate dummy training data
+x_train = np.random.random((1000, timesteps, data_dim))
+y_train = np.random.random((1000, nb_classes))
+
+# generate dummy validation data
+x_val = np.random.random((100, timesteps, data_dim))
+y_val = np.random.random((100, nb_classes))
+
+model.fit(x_train, y_train,
+          batch_size=64, nb_epoch=5,
+          validation_data=(x_val, y_val))
--- Tensorflow/tflearn test1.py
+++ Tensorflow/tflearn test1.py
@@ -1,59 +1,14 @@
-import numpy as np
+class foo():
+    def __init__(self,date):
+        self._date2 = date
 
-a = []
-a.append([0,1])
-a.append([1,0])
-
-a = np.array(a)
-print(a.shape)
+    @property
+    def getx(self):
+        # print(self.date2)
+        return self._date2
 
 
-# import tensorflow as tf
-# from tensorflow.models.rnn import rnn, rnn_cell
-# import numpy as np
-#
-# char_rdic = ['h', 'e', 'l', 'o'] # id -> char
-# char_dic = {w: i for i, w in enumerate(char_rdic)} # char -> id
-# x_data = np.array([[1,0,0,0], # h
-#          [0,1,0,0], # e
-#          [0,0,1,0], # l
-#          [0,0,1,0]], # l
-#          dtype='f')
-#
-# sample = [char_dic[c] for c in "hello"] # to index
-#
-# # Configuration
-# char_vocab_size = len(char_dic)
-# rnn_size = char_vocab_size  # 1 hot coding (on of 4)
-# time_step_size = 4 # 'hell' -> predict 'ello'
-# batch_size = 1 # one sample
-#
-#
-# # RNN model
-# rnn_cell = rnn_cell.BasicRNNCell(rnn_size)
-# state = tf.zeros([batch_size, rnn_cell.state_size]) # initial val : 0 !!
-# X_split = tf.split(0, time_step_size, x_data)
-# outputs, state = rnn.rnn(rnn_cell, X_split, state)
-#
-# # logits: list of 2D Tensors of shape [batch_size x num_decoder_symbols].
-# # targets: list of 1D batch-sized int32 Tensors of the same length as logits.
-# # weights: list of 1D batch=sized float-Tensors of the same lenght as logits.
-# logits = tf.reshape(tf.concat(1, outputs), [-1, rnn_size])
-# targets = tf.reshape(sample[1:], [-1])
-# weights = tf.ones([time_step_size * batch_size])
-#
-#
-# loss = tf.nn.seq2seq.sequence_loss_by_example([logits], [targets], [weights])
-# cost = tf.reduce_sum(loss) / batch_size
-# train_op = tf.train.RMSPropOptimizer(0.01, 0.9).minimize(cost)
-#
-#
-# # Launch the graph in a session
-# with tf.Session() as sess:
-#  # you need to initialize all variables
-#  tf.initialize_all_variables().run()
-#  for i in range(100):
-#   sess.run(train_op)
-#   result = sess.run(tf.arg_max(logits, 1))
-#   print (result, [char_rdic[t] for t in result])
-#
+a = foo(3)
+# a._date2 = 5
+print(a.getx)
+
